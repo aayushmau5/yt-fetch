5263f0c05dc2f948cce9858797a6cdc54ce7e092
Aayush Kumar Sahu <aayushmau5@gmail.com>
2021-12-29 01:42:00 +0530

    add more fields in database schema

--- db/Schema/index.js
+++ db/Schema/index.js
@@ -1,6 +1,31 @@
 const mongoose = require("mongoose");
 
 // the database object schema
-module.exports = new mongoose.Schema({
-  title: String,
-});
+module.exports = new mongoose.Schema(
+  {
+    title: String,
+    description: String,
+    channelId: String,
+    channelTitle: String,
+    videoId: String,
+    publishedAt: Date,
+    thumbnails: {
+      default: {
+        url: String,
+        width: Number,
+        height: Number,
+      },
+      medium: {
+        url: String,
+        width: Number,
+        height: Number,
+      },
+      high: {
+        url: String,
+        width: Number,
+        height: Number,
+      },
+    },
+  },
+  { timestamps: true }
+);
